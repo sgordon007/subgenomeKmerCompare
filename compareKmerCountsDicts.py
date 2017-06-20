efe51080931b71fb6a3e040aa41f233a27901ced
sgordon007 <seangordon07@gmail.com>
2017-06-20 16:15:11 -0700

    partly functional, testing functions and reading input

--- compareKmerCountsDicts.py
+++ compareKmerCountsDicts.py
@@ -3,10 +3,6 @@ import subprocess, shutil
 # parse config file
 configFile = open('kmerCountCompare.txt','r') #generate in integrated analysis
 
-# system path to find allmaps installation files
-path = parseConfigFindPath('systemPath',configFile)
-sortPath = parseConfigFindPath('SortPath',configFile)
-sortFiles = filter(None,str(subprocess.Popen(['ls', '%s' % sortPath], stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.read()).split('\n'))
 
 def parseConfigFindPath(stringFind,configFile):
     """findPath will find path of associated specified string"""
@@ -14,6 +10,12 @@ def parseConfigFindPath(stringFind,configFile):
         if stringFind in line: # if find string specified, return pathname/info
             configFile.seek(0)
             return line.split()[-1].strip('\n')
+
+# system path to find allmaps installation files
+path = parseConfigFindPath('systemPath',configFile)
+sortPath = parseConfigFindPath('SortPath',configFile)
+sortFiles = filter(None,str(subprocess.Popen(['ls', '%s' % sortPath], stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.read()).split('\n'))
+
 
 def kmercounttodict(sort2fname,sortPath):
     """Change sort2 into a dictionary object with keys being genes of species sort2, allows for ease of access of data"""
@@ -30,26 +32,26 @@ def kmercounttodict(sort2fname,sortPath):
 dictOfGenes = {}
 for file in sortFiles:
     # create dictionary of species, each species entry has their own sort file converted to dictionary for ease access
-    dictOfGenes[file.replace('q.','').replace('t.','').split('.')[1]] = kmercounttodict(file,sortPath)
+    dictOfGenes[file.split('.')[0]] = kmercounttodict(file,sortPath)
     # what does this print, test
-    print file.replace('q.', '').replace('t.', '').split('.')[1]
+    print file.split('.')[0]
 
 
 
-geneCount = {}
-for i in range(len(unOutSyntenies)):
-    # find query and target species names from unout files
-    qspeciesName = unOutSyntenies[i][unOutSyntenies[i].find('.')+1:unOutSyntenies[i].find('-')]
-    tspeciesName = unOutSyntenies[i][unOutSyntenies[i].find('_')+1:unOutSyntenies[i].rfind('_')].strip('0.')
-    # initialize genecounts for each species if doesn't already exist. If exists, continue gene count
-    if qspeciesName not in geneCount.keys():
-        geneCount[qspeciesName]=0
-    if tspeciesName not in geneCount.keys():
-        geneCount[tspeciesName]=0
-
-# output query and target file objects
-outq = open(outFileNames[0], 'w')
-outt = open(outFileNames[1], 'w')
+# geneCount = {}
+# for i in range(len(unOutSyntenies)):
+#     # find query and target species names from unout files
+#     qspeciesName = unOutSyntenies[i][unOutSyntenies[i].find('.')+1:unOutSyntenies[i].find('-')]
+#     tspeciesName = unOutSyntenies[i][unOutSyntenies[i].find('_')+1:unOutSyntenies[i].rfind('_')].strip('0.')
+#     # initialize genecounts for each species if doesn't already exist. If exists, continue gene count
+#     if qspeciesName not in geneCount.keys():
+#         geneCount[qspeciesName]=0
+#     if tspeciesName not in geneCount.keys():
+#         geneCount[tspeciesName]=0
+#
+# # output query and target file objects
+# outq = open(outFileNames[0], 'w')
+# outt = open(outFileNames[1], 'w')
 # for line in inputFile:
 #     if line:
 #         if '[' not in line and read == 1:  # Chr05N,3014546,Pavir.5NG471200.1,Chr05,1013018,Pahal.E01839.1	3:1	5	115	782	0.0	0.871794871794872
@@ -74,9 +76,9 @@ outt = open(outFileNames[1], 'w')
 #             if int(line.split()[-6]) >= lociThreshold:
 #                 read = 1
 
-outputFileGenemap.close()
-outq.close()
-outt.close()
-
-inputFile.close()
+# outputFileGenemap.close()
+# outq.close()
+# outt.close()
+#
+# inputFile.close()
 
